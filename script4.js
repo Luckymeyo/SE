c9bd4fd9ce3b0a80c4bc02fb72db9ade7b2bf7a3
Luckymeyo <52824270+Luckymeyo@users.noreply.github.com>
2024-06-11 19:49:18 +0700

    Update script4.js

--- script4.js
+++ script4.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const nextButton = document.getElementById('nextButton');
 
     nextButton.addEventListener('click', function() {
-        window.location.href = 'home.html';
+        window.location.href = 'Home.html';
     });
 
     document.querySelector('.back-btn').addEventListener('click', function() {
@@ -10,6 +10,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     document.querySelector('.close-btn').addEventListener('click', function() {
-        window.location.href = 'home.html';
+        window.location.href = 'Home.html';
     });
 });
